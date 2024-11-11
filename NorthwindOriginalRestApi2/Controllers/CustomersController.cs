18d872bd52f43d625104129feb450944efd34930
Amal Aden <amal.aden@student.careeria.fi>
2024-11-11 17:31:55 +0200

    HttpPut muokkaus ominaisuus ja try-catch toiminta Toimii hyvin

--- NorthwindOriginalRestApi2/Controllers/CustomersController.cs
+++ NorthwindOriginalRestApi2/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NorthwindOriginalRestApi2.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace NorthwindOriginalRestApi2.Controllers
 {
@@ -25,9 +26,9 @@ namespace NorthwindOriginalRestApi2.Controllers
                 var asiakkaat = db.Customers.ToList();
                 return Ok(asiakkaat);
             }
-            catch (Exception e) 
-            { 
-            return BadRequest("Tapahtui virhe. Lues lisää : " + e.InnerException);
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe. Lues lisää : " + e.InnerException);
             }
             // tehty try -catch ja Exception ex. innerexception vieti ketju 
         }
@@ -89,20 +90,60 @@ namespace NorthwindOriginalRestApi2.Controllers
                 {
                     db.Customers.Remove(asiakas);
                     db.SaveChanges();
-                    return Ok("Asiakas id.llä poistettiin " + asiakas.CompanyName );
+                    return Ok("Asiakas id.llä poistettiin " + asiakas.CompanyName);
 
                 }
-                else 
+                else
                 {
                     return NotFound("Asiakas id.llä " + id + " ei löydy");
                 }
 
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return BadRequest(e.InnerException);
             }
 
         }
+
+        [HttpPut("{id}")] // Asiakas tietojen muokaaminen 
+        public ActionResult EditCustomer(string id, [FromBody] Customer customer) // otetaan url. id ja http customer olio.
+        {
+            try
+            {
+                var asiakas = (db.Customers.Find(id));// ensi haetaan asiakas tietokannasta id perusteella käsittelyyn 
+                                                        // Muokavat omunaisuudet oliohin 
+
+                                                        // eli asiakas.CompanyName on ennestään oleva muutujaan joka on tallennettu
+                                                        // customer.CompanyName on parametrilla tullut muokattu asiakas
+                if (asiakas != null)
+                {
+                    asiakas.CompanyName = customer.CompanyName;
+                    asiakas.ContactName = customer.ContactName;
+                    asiakas.Address = customer.Address;
+                    asiakas.City = customer.City;
+                    asiakas.Region = customer.Region;
+                    asiakas.PostalCode = customer.PostalCode;
+                    asiakas.Country = customer.Country;
+                    asiakas.Phone = customer.Phone;
+                    asiakas.Fax = customer.Fax;
+
+                    db.SaveChanges();
+                    return Ok("Asiakas tiedot muokattu" + asiakas.CompanyName);
+                }
+                else
+                {
+                    return NotFound("Asiakasta ei löydetty id.llä " + id);
+                }
+
+
+            }
+            catch (Exception e) 
+            { 
+                return BadRequest("On tapantunut virhe. Lue lisää: " +e.InnerException);
+             }
+            
+        }
+
     }
 }
