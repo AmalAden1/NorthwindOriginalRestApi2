2264f44d8d9b97e72094b916b2a4a08b51b5d97b
Amal Aden <amal.aden@student.careeria.fi>
2024-11-11 14:24:35 +0200

    Httpget customers metodi try-catch loopilla Httget customer id metodi tray-catch loopilla uutena Exception e - virhekäsittely toiminta e.InnerException

--- NorthwindOriginalRestApi2/Controllers/CustomersController.cs
+++ NorthwindOriginalRestApi2/Controllers/CustomersController.cs
@@ -20,10 +20,44 @@ namespace NorthwindOriginalRestApi2.Controllers
         [HttpGet] // Haetaan kaikki asiakkaat Customer taulukosta ja listaksi.
         public ActionResult GetAllCustomers()
         {
-            var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            {
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+            }
+            catch (Exception e) 
+            { 
+            return BadRequest("Tapahtui virhe. Lues lisää : " + e.InnerException);
+            }
+            // tehty try -catch ja Exception ex. innerexception vieti ketju 
         }
-                                        
+
+
+
+
+        [HttpGet("{id}")] // haetaan yksi asiakas id perusteella
+        //[Route("{ id }")] url osoitepolun  laitettava parametri määrittely (vapaasti määriteltävä mikä lisätää metodin parametriksi)
+        public ActionResult GetOneCustomersById(string id)
+        {
+            try
+            {
+                var asiakas = db.Customers.Find(id);
+                if (asiakas != null) // ei ole null -> !=null 
+                {
+                    return Ok(asiakas);
+                }
+
+                else
+                {
+                    //  return BadRequest("Asikasta id:llä" + id + " ei löydy");
+                    return NotFound($"Asikasta id:llä {id} ei löydy");// string interpolation $ merkillä 
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest("On tapahtunut virhe. Lue lisää:" + e.InnerException);
+            }
+        }
 
     }
 }
